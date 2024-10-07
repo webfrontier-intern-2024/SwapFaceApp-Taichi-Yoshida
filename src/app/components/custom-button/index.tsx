496ec15991f563771b9008ebd1d607529a279881
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-08 03:54:23 +0900

    ボタンの押下可能不可能によって背景色が変わるように変更

--- src/app/components/custom-button/index.tsx
+++ src/app/components/custom-button/index.tsx
@@ -1,14 +1,17 @@
 interface CustomButtonProps {
     children: React.ReactNode;
     onClick: () => void;
+    disabled: boolean;
 }
-  
-export function CustomButton({ children, onClick }: CustomButtonProps) {
+
+export function CustomButton({ children, onClick, disabled = false }: CustomButtonProps) {
+    const bgColor = disabled ? 'bg-gray-400' : 'bg-blue-600';
     return (
         <div>
         <button
-            className="px-4 py-2 bg-blue-600 text-white rounded-lg"
+            className={`px-4 py-2 text-white rounded-lg ${bgColor} outline outline-gray-500`}
             onClick={onClick}
+            disabled={disabled}
         >
             <div className="flex justify-center">
             <span>{children}</span>
