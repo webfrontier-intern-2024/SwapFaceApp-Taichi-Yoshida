c134f7f4cb5c8484224e55d9cf75cef569a858a0
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-09 02:04:10 +0900

    APIのやり取り処理の追加

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  api: {
+    bodyParser: false, // formDataを扱うためにbodyParserを無効化
+  },
+};
+
+export async function POST(req: NextRequest) {
+  const apiUrl = process.env.NEXT_PUBLIC_COMPREFACE_URL || '';
+  const apiKey = process.env.NEXT_PUBLIC_COMPREFACE_KEY || '';  
+  console.log(apiUrl)
+  console.log(apiKey)
+  
+  try {
+    const formData = await req.formData();
+    const file = formData.get('file') as File | null;
+    console.log(formData)
+    if (!file) throw new Error('ファイルがアップロードされていません');
+
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: { 'x-api-key': apiKey },
+      body: formData,
+    });
+
+    if (!response.ok) throw new Error(response.statusText);
+
+    const result = await response.json();
+    return NextResponse.json({ message: '画像送信成功', result });
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : '不明なエラー';
+    console.error('エラーが発生しました:', errorMessage);
+    return NextResponse.json({ message: 'サーバーエラー', error: errorMessage }, { status: 500 });
+  }
+}
