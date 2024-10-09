97b26c979f9b861c9fb3f884906c9b23772f97f5
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-09 16:17:56 +0900

    API通信処理の追加

--- src/app/api/face/route.ts
+++ src/app/api/face/route.ts
@@ -8,9 +8,7 @@ export const config = {
 export async function POST(req: NextRequest) {
   const apiUrl = process.env.NEXT_PUBLIC_COMPREFACE_URL || '';
   const apiKey = process.env.NEXT_PUBLIC_COMPREFACE_KEY || '';  
-  console.log(apiUrl)
-  console.log(apiKey)
-  
+
   try {
     const formData = await req.formData();
     const file = formData.get('file') as File | null;
@@ -26,7 +24,10 @@ export async function POST(req: NextRequest) {
     if (!response.ok) throw new Error(response.statusText);
 
     const result = await response.json();
-    return NextResponse.json({ message: '成功したお', result });
+    const box = result.result?.[0].box;
+    console.log("HIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMA")
+    console.log('box data:', box);
+    return NextResponse.json({ message: '成功したお', box });
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : '不明なエラー';
     console.error('エラーが発生しました:', errorMessage);
