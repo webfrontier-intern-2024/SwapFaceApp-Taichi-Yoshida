6f8f30cdc0fb059f29b06c6c91762d4f88101800
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-10 16:44:12 +0900

    環境変数名の変更

--- src/app/api/face/route.ts
+++ src/app/api/face/route.ts
@@ -5,32 +5,24 @@ export const config = {
     bodyParser: false,
   },
 };
+
 export async function POST(req: NextRequest) {
-  const apiUrl = process.env.COMPREFACE_URL || '';
-  const apiKey = process.env.COMPREFACE_KEY || '';  
+  const apiUrl = process.env.HIMAZIN_COMPREFACE_URL || '';
+  const apiKey = process.env.HIMAZIN_COMPREFACE_KEY || '';  
 
   try {
     const formData = await req.formData();
-    //const file = formData.get('file') as File | null;
-    //console.log(formData)
-    //if (!file) throw new Error('ファイルがアップロードされていません');
-
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {'x-api-key': apiKey },
       body: formData,
     });
-
     if (!response.ok) throw new Error(response.statusText);
-
     const result = await response.json();
     const box = result.result?.[0].box;
-    console.log("HIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMAHIMA")
-    console.log('box data:', box);
-    return NextResponse.json({ message: '成功したお', box });
+    return NextResponse.json({ message: '成功したおoooo', box });
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : '不明なエラー';
-    console.error('エラーが発生しました:', errorMessage);
-    return NextResponse.json({ message: 'サーバーエラー', error: errorMessage }, { status: 500 });
+    return NextResponse.json({ message: '顔を認識することが出来ませんでした。', error: errorMessage }, { status: 500 });
   }
 }
