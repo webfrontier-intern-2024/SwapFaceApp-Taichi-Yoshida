b56c451f29d0c4008b0645cfe785626be2355902
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-10 16:44:44 +0900

    lottieの追加

---
+++ src/app/components/lottie/file-up.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Player } from "@lottiefiles/react-lottie-player";
+
+export function FileUp() {
+  return <Player src="/lotties/file-up.json" autoplay loop style={{ width: '100px', height: '100px' }}/>;
+}
---
+++ src/app/components/lottie/light-arrow.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Player } from "@lottiefiles/react-lottie-player";
+
+export function LightArrow() {
+  return <Player src="/lotties/light-arrow.json" autoplay loop style={{ width: '300px', height: '300px' }} className="m-10"/>;
+}
