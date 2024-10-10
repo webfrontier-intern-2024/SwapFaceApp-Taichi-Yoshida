9fb7032b3b8458895d5283905bf271c7010d8a28
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-10 17:07:07 +0900

    lottieのサイズ変更

--- src/app/components/lottie/light-arrow.tsx
+++ src/app/components/lottie/light-arrow.tsx
@@ -3,5 +3,5 @@
 import { Player } from "@lottiefiles/react-lottie-player";
 
 export function LightArrow() {
-  return <Player src="/lotties/light-arrow.json" autoplay loop style={{ width: '300px', height: '300px' }} className="m-10"/>;
+  return <Player src="/lotties/light-arrow.json" autoplay loop style={{ width: '200px', height: '200px' }} className="m-5"/>;
 }
