abc250a7453274162978403f22395a22e9716b89
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-10 16:44:27 +0900

    ボタンデザインの変更

--- src/app/components/custom-button/index.tsx
+++ src/app/components/custom-button/index.tsx
@@ -2,22 +2,20 @@ interface CustomButtonProps {
     children: React.ReactNode;
     onClick: () => void;
     disabled: boolean;
+    lightColor : string;
+    darkColor:string;
 }
 
-export function CustomButton({ children, onClick, disabled = false }: CustomButtonProps) {
-    const bgColor = disabled ? 'bg-gray-400' : 'bg-blue-600';
+export function CustomButton({ children, onClick, disabled = false, lightColor, darkColor }: CustomButtonProps) {
+    const bgLightColor = disabled ? 'bg-gray-500' : lightColor;
+    const bgdDarkColor = disabled ? 'bg-gray-600' : darkColor;
     return (
-        <div>
-        <button
-            className={`px-4 py-2 text-white rounded-lg ${bgColor} outline outline-gray-500`}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            <div className="flex justify-center">
-            <span>{children}</span>
-            </div>
+        <button className={`group relative h-12 overflow-hidden overflow-x-hidden rounded-md ${bgLightColor} px-4 py-2 text-neutral-50`} onClick={onClick} disabled={disabled}>
+            <span className="relative z-10">{children}</span>
+            <span className="absolute inset-0 overflow-hidden rounded-md">
+                <span className={`absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full ${bgdDarkColor} transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150`}>
+                </span>
+            </span>
         </button>
-        </div>
     );
 }
-  
